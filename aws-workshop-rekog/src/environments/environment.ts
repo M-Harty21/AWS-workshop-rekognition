e517c7cd5315e83c5c635a248570fc2d9347702b
Matt Harty <matthewnharty@gmail.com>
2022-06-06 19:19:01 +0200

    Fixed something

--- aws-workshop-rekog/src/environments/environment.ts
+++ aws-workshop-rekog/src/environments/environment.ts
@@ -3,6 +3,4 @@ export const environment = {
   production: false,
   // ACCESS_KEY_ID: '', // removed for security purposes 
   // SECRET_KEY: ''  // removed for security purposes
-  ACCESS_KEY_ID: 'AKIAUVQOEPQJAROVUBXK',
-  SECRET_KEY: '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'
 };
